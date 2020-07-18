232d7e65c34dff65f18c0e9c1d3161a0344c71a9
chriszer tamayo <chriszer.myth@gmail.com>
2020-07-18 21:34:58 +0800

    all api done

---
+++ server/index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+
+const config = require('./config/key.js')
+
+const { User } = require('./models/user')
+const { auth } = require('./middleware/auth')
+
+mongoose.connect(config.mongoURI,
+{ useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true }).then(()=>{
+    console.log('DB connected')
+}).catch(err => console.log(err))
+
+
+app.use(express.json())
+app.use(cookieParser())
+
+
+app.get('/',(req,res) => {
+    res.json({
+        message:"hello world"
+    })
+   
+})
+
+
+app.get("/api/user/auth",auth,(req,res) => {
+    res.status(200).json({
+        _id:req._id,
+        isAuth: true,
+        email:req.user.email,
+        name: req.user.name,
+        lastname:req.user.lastname,
+        rolse: req.user.role
+    })
+})
+
+app.post('/api/users/register',(req,res) => {
+    const user = new User(req.body)
+
+    user.save((err,doc) => {
+        if(err) return res.json({ success: false, err})
+        res.status(200).json({
+            success:true,
+            userData: doc
+        })
+    })
+})
+
+
+    app.post('/api/user/login',(req,res) => {
+    //find the email
+    User.findOne({ email: req.body.email },(err,user) => {
+        if(!user)
+        return res.json({
+            loginSuccess: false,
+            message: "Auth failed, email not found"
+        })
+         //comparePassword
+
+         user.comparePassword(req.body.password,(err,isMatch)=>{
+             if(!isMatch){
+                 return res.json ({loginSuccess: false, message:"wrong password"})
+             }
+         })
+
+          //generateToken
+          user.generateToken((err, user) => {
+            if(err) return res.status(400).send(err)
+            res.cookie("x_auth",user.token)
+            .status(200)
+            .json({
+                loginSuccess: true
+            })
+          })
+    })
+   
+})
+
+     app.get("/api/user/logout", auth , (req, res) => {
+         User.findOneAndUpdate({_id: req.user._id}, {token:""},(err,doc)=> {
+             if(err) return res.json({ success: false, err})
+             return res.status(200).send({
+                 success: true
+             })
+         })
+     })
+
+
+app.listen(5000,()=>console.log('listening to port 5000'));
